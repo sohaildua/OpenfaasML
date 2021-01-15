385924a141ac836ae347b98a5bdd33cc276788d6
Sdgamer007 <sdgamer007@sdgamer007>
2021-01-14 19:26:24 -0500

    new face function

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def face():
     return data
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5002)))
